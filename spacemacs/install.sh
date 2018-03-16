fc9620990c265f24d61b93c452676533451194ab
Henrik <henrik.larsson@teliacompany.com>
2018-03-16 10:34:22 +0100

    Added install info for spaemacs

--- spacemacs/install.sh
+++ spacemacs/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+#git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Removing ~/.spacemacs"
 rm ~/.spacemacs
